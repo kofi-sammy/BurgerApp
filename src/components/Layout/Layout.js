47f1751d2bcd45747920058c5f7f5e96bdb16ce7
Samuel Boateng <sammydev56@gmail.com>
2021-12-16 21:18:15 +0100

    Addition of a Toolbar and SideDrawer Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-import classes from './Layout.module.css';
+import styles from './Layout.module.css';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../SideDrawer/SideDrawer";
 
 const Layout = (props) => {
     return(
     <React.Fragment>
         <div>
-            Toolbar,
-            SideDrawer,
+            <Toolbar/>
+            <SideDrawer/>
             Backdrop
         </div>
         
-            <main className= {classes.Content} >
+            <main className= {styles.Content} >
                 {props.children}
             </main>
     </React.Fragment>
