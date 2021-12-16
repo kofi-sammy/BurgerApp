fbff7362fc42ff4bbc1e8195a8aba4ac1f31660a
Samuel Boateng <sammydev56@gmail.com>
2021-12-16 21:29:21 +0100

    Added an OrderSummary component to get the order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../../UI/Button/Button';
+import BuildControls from '../BuildControls/BuildControls';
+
+ const OrderSummary =(props)=> {
+    const ingredientSummary = Object.keys(props.ingredients)
+    .map(
+        igKey => { 
+            return (
+                    <li key={igKey}>
+                      <span style={{textTransform:"capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
+                    </li>
+            );
+        } 
+        
+    )
+    
+    return (
+     <React.Fragment>
+     
+         <h3>Your Order</h3>
+         <p>A delicious burger with the following ingredients: </p>
+         <p><strong>Current Price: {props.price.toFixed(2)}</strong></p>
+         <ul>{ingredientSummary}</ul>
+         <p>Continue to checkout? </p>
+         <Button clicked={props.purchaseCancelled} btnType='Danger'>CANCEL</Button>
+         <Button clicked={props.purchaseContinued} btnType='Success'>CONTINUE</Button>
+     </React.Fragment>
+    )
+}
+
+export default OrderSummary;
